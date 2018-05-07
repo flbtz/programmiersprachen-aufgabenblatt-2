a307fb789caa53756f01ca8e7e931db5ae662acd
Florian B <florian_batze@gmx.de>
2018-05-07 14:33:38 +0200

    added Mat_mult,det,inv,trans,bog
    added functions for mul,inv,trans,bog of 2x2 Matrix

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -1,52 +1,74 @@
 #ifndef MAT2_CPP
 #define MAT2_CPP
 
-#include"Mat2.hpp"
-#include"Vec2.hpp"
+#include"mat2.hpp"
+#include"vec2.hpp"
+#include<iostream>
 #include <math.h>
 
-Mat2::Mat2() : firstRow_(Vec2{ 1.0f, 0.0f }), secondRow_(Vec2{ 0.0f, 1.0f }) {};
+Mat2::Mat2():
+        a{Vec2{1.0f,0.0f}},
+        b{Vec2{0.0f,1.0f}} {}
 
-Mat2::Mat2(Vec2 const& firstRow, Vec2 const& secondRow): firstRow_(firstRow), secondRow_(secondRow) {}
+Mat2::Mat2(Vec2 const& a1, Vec2 const& b1):
+        a{a1},
+        b{b1}
+{}
 
-Mat2& Mat2::operator*=(Mat2 const& m) {
-    Vec2 newFirstRow_ = { firstRow_.x * m.firstRow_.x + secondRow_.x * m.firstRow_.y, firstRow_.y * m.firstRow_.x + secondRow_.y * m.firstRow_.y};
-    Vec2 newSecondRow_ = {firstRow_.x * m.secondRow_.x + secondRow_.x * m.secondRow_.y, firstRow_.y * m.secondRow_.x + secondRow_.y * m.secondRow_.y};
-    firstRow_ = newFirstRow_;
-    secondRow_ = newSecondRow_;
+
+Mat2 & Mat2::operator *=( Mat2 const & m ){
+    Vec2 a_temp1 = {a.x * m.a.x + b.x * m.a.y, a.y * m.a.x + b.y *m.a.y};
+    Vec2 b_temp1 = {a.x * m.b.x + b.x * m.b.y, a.y * m.b.x + b.y *m.b.y};
+
+    a = a_temp1;
+    b = b_temp1;
     return *this;
 };
 
-float Mat2::det() const {
-    return firstRow_.x * secondRow_.y - firstRow_.y * secondRow_.x;
+
+float Mat2::det () const {
+    return a.x * b.y - a.y * b.x;
+
 }
 
-Mat2 operator*(Mat2 const& m1, Mat2 const& m2) {
+
+
+
+
+Mat2 operator *( Mat2 const & m1 , Mat2 const & m2 ){
     return Mat2{
-            { m1.firstRow_.x * m2.firstRow_.x + m1.secondRow_.x * m2.firstRow_.y, m1.firstRow_.y * m2.firstRow_.x + m1.secondRow_.y * m2.firstRow_.y },
-            { m1.firstRow_.x * m2.secondRow_.x + m1.secondRow_.x * m2.secondRow_.y, m1.firstRow_.y * m2.secondRow_.x + m1.secondRow_.y * m2.secondRow_.y }
+            { m1.a.x * m2.a.x + m1.b.x * m2.a.y, m1.a.y * m2.a.x + m1.b.y * m2.a.y },
+            { m1.a.x * m2.b.x + m1.b.x * m2.b.y, m1.a.y * m2.b.x + m1.b.y * m2.b.y }
     };
 }
 
-Vec2 operator*(Mat2 const& m, Vec2 const& v) {
-    return { m.firstRow_.x * v.x + m.secondRow_.x * v.y, m.firstRow_.y * v.x + m.secondRow_.y * v.y };
-};
+Vec2 operator *( Mat2 const & m , Vec2 const & v ){
+    return { m.a.x * v.x + m.b.x * v.y, m.a.y * v.x + m.b.y * v.y };
 
-Vec2 operator*(Vec2 const& v, Mat2 const& m) {
-    return { m.firstRow_.x * v.x + m.secondRow_.x * v.y, m.firstRow_.y * v.x + m.secondRow_.y * v.y };
 }
 
-Mat2 inverse(Mat2 const& m) {
-    float a = m.firstRow_.x * m.secondRow_.y - m.secondRow_.x * m.firstRow_.y;
-    return { { m.secondRow_.y / a , - m.firstRow_.y / a} , { - m.secondRow_.x / a , m.firstRow_.x / a } };
+
+Vec2 operator *( Vec2 const & v , Mat2 const & m )
+{
+    return { m.a.x * v.x + m.b.x * v.y, m.a.y * v.x + m.b.y * v.y };
 }
 
-Mat2 transpose(Mat2 const& m) {
-    return { {m.firstRow_.x,m.secondRow_.x},{m.firstRow_.y,m.secondRow_.y} };
+Mat2 inverse ( Mat2 const & m )
+{
+    float i = m.a.x * m.b.y - m.b.x * m.a.y;
+    return {{m.b.y / i, -m.a.y/i},{-m.b.x/i ,m.a.x/i}};
+
 }
 
-Mat2 make_rotation_mat2(float phi) {
+Mat2 transpose ( Mat2 const & m )
+{
+    return { {m.a.x,m.b.x},{m.a.y,m.b.y} };
+}
+
+Mat2 make_rotation_mat2 ( float phi )
+{
     return { { (float)cos(phi), (float)sin(phi)}, { (float)-sin(phi) , (float)cos(phi)} };
 }
 
-#endif
+
+# endif
--- source/tests.cpp
+++ source/tests.cpp
@@ -1,136 +1,140 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 #include "vec2.hpp"
+#include "mat2.hpp"
+
+
 
 int main(int argc, char *argv[])
 {
-  return Catch::Session().run(argc, argv);
+    return Catch::Session().run(argc, argv);
 }
 
-TEST_CASE ("describe_Vec2","[Vec2]")
+
+TEST_CASE ( " describe_Vec2" ,"[Vec2]")
 {
-  Vec2 v1 = Vec2{};
-  Vec2 v2 = Vec2{3.4f,6.5f};
-  Vec2 v3 = Vec2{15.0f,3.0f};
+    Vec2 v1 = Vec2{};
+    Vec2 v2 = Vec2{2.4f,3.5f};
+    Vec2 v3 = Vec2{10.0f,4.0f};
 
-  SECTION("test_default_constructor"){
+    SECTION("test_default_constructor"){
 
-    REQUIRE (v1.x ==0.0f);
-    REQUIRE (v1.y ==0.0f);
-  }
-  SECTION("test_parameter_constructor"){
+        REQUIRE (v1.x ==0.0f);
+        REQUIRE (v1.y ==0.0f);
+    }
+    SECTION("test_parameter_constructor"){
 
-    REQUIRE (v2.x ==2.4f);
-    REQUIRE (v2.y ==3.5f);
+        REQUIRE (v2.x ==2.4f);
+        REQUIRE (v2.y ==3.5f);
 
-  }
-  SECTION("test_operator+="){
-
-
-    v1 +=v2;
-    v2 +=v3;
-
-    REQUIRE (v1.x == 4.4f);
-    REQUIRE (v1.y == 2.5f);
-    REQUIRE (v2.x == 9.4f);
-    REQUIRE (v2.y == 12.5f);
+    }
+    SECTION("test_operator+="){
 
 
-  }
+        v1 +=v2;
+        v2 +=v3;
 
-  SECTION("test_operator-="){
-
-    v1 -=v2;
-    v2 -=v3;
-
-    REQUIRE (v1.x == -4.4f);
-    REQUIRE (v1.y == -5.5f);
-    REQUIRE (v2.x == -8.5f);
-    REQUIRE (v2.y == -0.8f);
+        REQUIRE (v1.x == 2.4f);
+        REQUIRE (v1.y == 3.5f);
+        REQUIRE (v2.x == 12.4f);
+        REQUIRE (v2.y == 7.5f);
 
 
-  }
+    }
 
-  SECTION("test_operator*="){
-    v1 *= 5.0f;
-    v3 *= 5.0f;
+    SECTION("test_operator-="){
 
-    REQUIRE (v1.x == 0.0f);
-    REQUIRE (v1.y == 0.0f);
-    REQUIRE (v3.x == 50.0f);
-    REQUIRE (v3.y == 20.0f);
+        v1 -=v2;
+        v2 -=v3;
 
-
-  }
-  SECTION("test_operator/="){
-    v1 /= 5.0f;
-    v3 /= 2.0f;
-
-    REQUIRE (v1.x == 0.0f);
-    REQUIRE (v1.y == 0.0f);
-    REQUIRE (v3.x == 5.0f);
-    REQUIRE (v3.y == 2.0f);
+        REQUIRE (v1.x == -2.4f);
+        REQUIRE (v1.y == -3.5f);
+        REQUIRE (v2.x == -7.6f);
+        REQUIRE (v2.y == -0.5f);
 
 
-  }
+    }
 
-  SECTION("test_operator+")
-  {
-    Vec2 s1 = {3.5f,7.0f};
-    Vec2 s2 = {4.4f,1.6f};
-    Vec2 sum1 = s1+s2;
+    SECTION("test_operator*="){
+        v1 *= 5.0f;
+        v3 *= 5.0f;
 
-    REQUIRE (sum1.x ==6.9f);
-    REQUIRE (sum1.y ==10.6f);
-  }
-
-  SECTION("test_operator-"){
-
-    Vec2 d1 = {3.5f,0.0f};
-    Vec2 d2 = {4.6f,3.4f};
-    Vec2 subtr1 = d1-d2;
+        REQUIRE (v1.x == 0.0f);
+        REQUIRE (v1.y == 0.0f);
+        REQUIRE (v3.x == 50.0f);
+        REQUIRE (v3.y == 20.0f);
 
 
-    REQUIRE (subtr1.y ==-3.4f);
-    REQUIRE (subtr1.x ==3.1f);
+    }
+    SECTION("test_operator/="){
+        v1 /= 5.0f;
+        v3 /= 2.0f;
 
-  }
-
-  SECTION("test_operator*(1)"){
-
-    Vec2 f1 = {4.5f,5.0f};
-    float f2 = {2.0f};
-    Vec2 prd1 = f1*f2;
-
-    REQUIRE (prd1.x ==15.5f);
-    REQUIRE (prd1.y ==18.0f);
-  }
+        REQUIRE (v1.x == 0.0f);
+        REQUIRE (v1.y == 0.0f);
+        REQUIRE (v3.x == 5.0f);
+        REQUIRE (v3.y == 2.0f);
 
 
-  SECTION("test_operator/"){
+    }
 
-    Vec2 div1 = {11.0f,4.0f};
-    float div2 = {5.0f};
-    Vec2 erg1 = div1/div2;
-    Vec2 div3 = {11.0f,1.0f};
-    float div4 = {0.0f};
-    Vec2 erg2 = div3/div4;
+    SECTION("test_operator+")
+    {
+        Vec2 s1 = {5.5f,6.0f};
+        Vec2 s2 = {1.4f,4.6f};
+        Vec2 sum1 = s1+s2;
 
-    REQUIRE (erg1.x ==3.0f);
-    REQUIRE (erg1.y ==5.0f);
-    REQUIRE (erg2.x ==9.0f);
-    REQUIRE (erg2.y ==2.0f);
-  }
+        REQUIRE (sum1.x ==6.9f);
+        REQUIRE (sum1.y ==10.6f);
+    }
 
-  SECTION("test_operator*(2)"){
+    SECTION("test_operator-"){
 
-    Vec2 f2 = {3.5f,5.0f};
-    float f1 = {4.0f};
-    Vec2 prd2 = f1*f2;
+        Vec2 d1 = {5.5f,0.0f};
+        Vec2 d2 = {1.4f,4.4f};
+        Vec2 subtr1 = d1-d2;
 
-    REQUIRE (prd2.x ==15.5f);
-    REQUIRE (prd2.y ==16.0f);
-  }
+
+        REQUIRE (subtr1.y ==-4.4f);
+        REQUIRE (subtr1.x ==4.1f);
+
+    }
+
+    SECTION("test_operator*(1)"){
+
+        Vec2 f1 = {5.5f,6.0f};
+        float f2 = {3.0f};
+        Vec2 prd1 = f1*f2;
+
+        REQUIRE (prd1.x ==16.5f);
+        REQUIRE (prd1.y ==18.0f);
+    }
+
+
+    SECTION("test_operator/"){
+
+        Vec2 div1 = {12.0f,6.0f};
+        float div2 = {3.0f};
+        Vec2 erg1 = div1/div2;
+        Vec2 div3 = {10.0f,1.0f};
+        float div4 = {0.0f};
+        Vec2 erg2 = div3/div4;
+
+        REQUIRE (erg1.x ==4.0f);
+        REQUIRE (erg1.y ==2.0f);
+        REQUIRE (erg2.x ==10.0f);
+        REQUIRE (erg2.y ==1.0f);
+    }
+
+    SECTION("test_operator*(2)"){
+
+        Vec2 f2 = {5.5f,6.0f};
+        float f1 = {3.0f};
+        Vec2 prd2 = f1*f2;
+
+        REQUIRE (prd2.x ==16.5f);
+        REQUIRE (prd2.y ==18.0f);
+    }
 
 }
 
